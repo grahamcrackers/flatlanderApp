9456a5331226ba9c3c6c5e000f2a01ddd63f4cc5
grahamcrackers <gmrogers04@gmail.com>
2014-12-22 11:53:17 -0600

    finished level 1

--- app/app.js
+++ app/app.js
@@ -5,13 +5,23 @@
   angular.module('gemStore', [ ])
 
       .controller('StoreController', function(){
-        this.product = gem;
+        this.products = gems;
       });
 
-  var gem = {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: '. . .',
-  }
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: 'Some gems have hiden qualities beyond their luster, beyond their shine... Dodeca is one of those gems',
+      canPurchase: true,
+      soldOut: false,
+    },
+    {
+      name: 'Pentagonal Gem',
+      price: 5.95,
+      description: 'Some gems have hiden qualities beyond their luster, beyond their shine... Dodeca is one of those gems',
+      canPurchase: true
 
+    }
+  ];
 })();
