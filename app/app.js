b90fa2e8ca741a2f0c60fdedebc2fa84b5e483ba
grahamcrackers <gmrogers04@gmail.com>
2014-12-22 14:45:50 -0600

    finished 2.4 section

--- app/app.js
+++ app/app.js
@@ -10,18 +10,47 @@
 
   var gems = [
     {
-      name: 'Dodecahedron',
-      price: 2.95,
+      name: 'Azurite',
       description: 'Some gems have hiden qualities beyond their luster, beyond their shine... Dodeca is one of those gems',
+      shine: 8,
+      price: 110.50,
+      rarity: 7,
+      color: '#CCC',
+      faces: 14,
+      images: [
+          "images/gem-02.gif",
+          "images/gem-05.gif",
+          "images/gem-09.gif"
+      ],
       canPurchase: true,
-      soldOut: false,
+      soldOut: false
     },
     {
-      name: 'Pentagonal Gem',
-      price: 5.95,
-      description: 'Some gems have hiden qualities beyond their luster, beyond their shine... Dodeca is one of those gems',
-      canPurchase: true
+      name: 'Bloodstone',
+      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+      shine: 9,
+      price: 22.90,
+      rarity: 6,
+      color: '#EEE',
+      faces: 12,
+      images: [
+        "images/gem-01.gif",
+        "images/gem-03.gif",
+        "images/gem-04.gif"
+      ]
+    }, {
+      name: 'Zircon',
+      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+      shine: 70,
+      price: 1100,
+      rarity: 2,
+      color: '#000',
+      faces: 6,
+      images: [
+        "images/gem-06.gif",
+        "images/gem-07.gif",
+        "images/gem-10.gif"
+      ]
+    }];
 
-    }
-  ];
 })();
